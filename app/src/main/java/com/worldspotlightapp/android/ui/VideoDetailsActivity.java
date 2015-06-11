7c8686ee7151d21e1de237bf6ab1be84a681e237
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2015-06-12 03:26:33 +0400

    Added Youtube android player library. Set the title, description and the back button in the video details activity

--- app/src/main/java/com/worldspotlightapp/android/ui/VideoDetailsActivity.java
+++ app/src/main/java/com/worldspotlightapp/android/ui/VideoDetailsActivity.java
@@ -2,7 +2,11 @@ package com.worldspotlightapp.android.ui;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 import com.worldspotlightapp.android.R;
 import com.worldspotlightapp.android.maincontroller.modules.ParseResponse;
 import com.worldspotlightapp.android.maincontroller.modules.videosmodule.VideosModuleObserver;
@@ -15,6 +19,7 @@ import java.util.Observable;
 public class VideoDetailsActivity extends AbstractBaseActivityObserver {
 
     private static final String TAG = "VideoDetailsActivity";
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
 
     private static final String GOOGLE_API = "AIzaSyB4nkNAMhIa-JXE3lxqPyk9GaEQqhpn_Q8";
 
@@ -22,6 +27,15 @@ public class VideoDetailsActivity extends AbstractBaseActivityObserver {
 
     private ParseResponse mParseResponse;
     private Video mVideo;
+
+    // Views
+    private TextView mDescriptionTextView;
+
+    private YouTubePlayerSupportFragment mYoutubePlayerFragment;
+    private YouTubePlayer mYouTubePlayer;
+
+    // Check if it was full screen or not
+    private boolean mIsFullScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +48,13 @@ public class VideoDetailsActivity extends AbstractBaseActivityObserver {
             throw new IllegalArgumentException("You must pass the video id using intent");
         }
         mVideoObjectId = extras.getString(Video.INTENT_KEY_OBJECT_ID);
+
+        // Action bar
+//        mActionBar.setHomeButtonEnabled(true);
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Link the views
+        mDescriptionTextView = (TextView) findViewById(R.id.description_text_view);
 
         // Retrieve the data
         mNotificationModule.showLoadingDialog(mContext);
@@ -87,7 +108,21 @@ public class VideoDetailsActivity extends AbstractBaseActivityObserver {
      * Show details about the video
      */
     private void showVideoDetails() {
-        // TODO: Implement this
         Log.v(TAG, mVideo.toString());
+
+        mActionBar.setTitle(mVideo.getTitle());
+        mDescriptionTextView.setText(mVideo.getDescription());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Log.v(TAG, "home button pressed");
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
