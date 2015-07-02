b2265d91f6745c45a79952e197df00677f07f243
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2015-07-02 22:48:58 +0400

    added the missing screens

---
+++ app/src/main/java/com/worldspotlightapp/android/ui/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.worldspotlightapp.android.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.worldspotlightapp.android.R;
+
+public class LoginActivity extends AbstractBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
