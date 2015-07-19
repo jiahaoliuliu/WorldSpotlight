2e326128be9062bf22d0e1c4dc8102a677d8583f
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2015-07-19 15:49:56 +0400

    Added the back button (home) in the actionbar for sign up and login

--- app/src/main/java/com/worldspotlightapp/android/ui/SignUpActivity.java
+++ app/src/main/java/com/worldspotlightapp/android/ui/SignUpActivity.java
@@ -1,8 +1,7 @@
 package com.worldspotlightapp.android.ui;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.worldspotlightapp.android.R;
@@ -11,10 +10,16 @@ import java.util.Observable;
 
 public class SignUpActivity extends AbstractBaseActivityObserver {
 
+    private static final String TAG = "SignUpActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        // Action bar
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 
     @Override
@@ -25,5 +30,17 @@ public class SignUpActivity extends AbstractBaseActivityObserver {
     @Override
     public void update(Observable observable, Object data) {
         // TODO: Implement this
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Log.v(TAG, "home button pressed");
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
