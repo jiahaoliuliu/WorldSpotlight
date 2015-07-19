ee1be65f91f325a2e729e1c714eff99dcc15d11b
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2015-07-19 18:19:45 +0400

    Correctly check the user name field for Sign in

--- app/src/main/java/com/worldspotlightapp/android/ui/SignUpActivity.java
+++ app/src/main/java/com/worldspotlightapp/android/ui/SignUpActivity.java
@@ -1,8 +1,12 @@
 package com.worldspotlightapp.android.ui;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.worldspotlightapp.android.R;
 
@@ -12,6 +16,14 @@ public class SignUpActivity extends AbstractBaseActivityObserver {
 
     private static final String TAG = "SignUpActivity";
 
+    private static final int MINIMUM_PASSWORD_LENGTH = 6;
+
+    // Views
+    private EditText mUserNameEditText;
+    private EditText mPasswordEditText;
+    private EditText mVerifyPasswordEditText;
+    private Button mSignUpButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +32,68 @@ public class SignUpActivity extends AbstractBaseActivityObserver {
         // Action bar
         mActionBar.setDisplayHomeAsUpEnabled(true);
 
+        // Link the views
+        mUserNameEditText = (EditText) findViewById(R.id.user_name_edit_text);
+        mPasswordEditText = (EditText) findViewById(R.id.password_edit_text);
+        mVerifyPasswordEditText = (EditText) findViewById(R.id.verify_password_edit_text);
+
+        mSignUpButton = (Button) findViewById(R.id.sign_up_button);
+        mSignUpButton.setOnClickListener(onClickListener);
+    }
+
+    private View.OnClickListener onClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.sign_up_button:
+                    signUp();
+                    break;
+            }
+        }
+    };
+
+    /**
+     * Try to sign up with the data introduced by the user
+     *
+     */
+    private void signUp() {
+        if (!areAllFieldsOk()) {
+            return;
+        }
+    }
+
+    /**
+     * Check if all the fields are ok.
+     * - The user name cannot be empty
+     * - The user name must be an email address
+     * - The password must has more than MINIMUM_PASSWORD_LENGTH characters
+     * - The password should match with the password in verify field
+     * @return
+     */
+    private boolean areAllFieldsOk() {
+
+        // To save the results of the checkings
+        boolean areAllFieldsOk = true;
+
+        // Username
+        String username = mUserNameEditText.getText().toString();
+        //      It cannot be empty
+        if (TextUtils.isEmpty(username)) {
+            mUserNameEditText.setError(getString(R.string.error_message_field_empty));
+            areAllFieldsOk = false;
+        //      It should have at least MINIMUM_PASSWORD_LENGTH characters
+        } else if (username.length() < MINIMUM_PASSWORD_LENGTH) {
+            mUserNameEditText.setError(getString(R.string.error_message_string_too_short, MINIMUM_PASSWORD_LENGTH));
+            areAllFieldsOk = false;
+        //      It must be an email address
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(username).matches()) {
+            mUserNameEditText.setError(getString(R.string.error_message_email_address_required));
+            areAllFieldsOk = false;
+        } else {
+            mUserNameEditText.setError(null);
+        }
+
+        return areAllFieldsOk;
     }
 
     @Override
