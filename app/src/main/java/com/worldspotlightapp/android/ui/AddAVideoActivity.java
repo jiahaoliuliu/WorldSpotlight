c62c6d15427b9ab68492a0f4d7dcd62cb2c143f1
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2015-08-03 23:09:09 +0400

    Added the needed elements in the action bar

--- app/src/main/java/com/worldspotlightapp/android/ui/AddAVideoActivity.java
+++ app/src/main/java/com/worldspotlightapp/android/ui/AddAVideoActivity.java
@@ -2,6 +2,8 @@ package com.worldspotlightapp.android.ui;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.worldspotlightapp.android.R;
 import com.worldspotlightapp.android.model.Video;
@@ -11,6 +13,8 @@ import java.util.Observable;
 public class AddAVideoActivity extends AbstractBaseActivityObserver {
 
     private static final String TAG = "AddAVideoActivity";
+
+    private static final int MENU_ITEM_ADD_A_VIDEO_ID = 1000;
 
     /**
      * The YouTube video id of the video to be added
@@ -31,6 +35,10 @@ public class AddAVideoActivity extends AbstractBaseActivityObserver {
         Log.v(TAG, "Video id received " + mVideoId);
 
         setContentView(R.layout.activity_add_a_video);
+
+        // Action bar
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 
     @Override
@@ -41,5 +49,35 @@ public class AddAVideoActivity extends AbstractBaseActivityObserver {
     @Override
     public void update(Observable observable, Object data) {
         // TODO: Implement this
+    }
+
+    // Action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Update user profile
+        MenuItem menuItemAddAVideo = menu.add(Menu.NONE, MENU_ITEM_ADD_A_VIDEO_ID, Menu
+                .NONE, R.string.action_bar_done)
+                .setIcon(R.drawable.ic_action_done);
+        menuItemAddAVideo.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Log.v(TAG, "home button pressed");
+                onBackPressed();
+                return true;
+            case MENU_ITEM_ADD_A_VIDEO_ID:
+                addThisVideo();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void addThisVideo() {
+        // TDOO: Implement this
     }
 }
