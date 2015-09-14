1b621c1f4c74e8d35ab6d76ca3df8ebe35876635
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2015-09-14 18:43:50 +0400

    Passing correctly the object id or the organizer from the organizer list to the organizer details activity

--- app/src/main/java/com/worldspotlightapp/android/ui/OrganizersRecyclerAdapter.java
+++ app/src/main/java/com/worldspotlightapp/android/ui/OrganizersRecyclerAdapter.java
@@ -1,7 +1,11 @@
 package com.worldspotlightapp.android.ui;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +23,8 @@ import java.util.List;
  */
 public class OrganizersRecyclerAdapter extends RecyclerView.Adapter<OrganizersRecyclerAdapter.ViewHolder> {
 
+    private static final String TAG = "OrganizersAdapter";
+
     private Context mContext;
     private Picasso mPicasso;
     private List<Organizer> mOrganizersList;
@@ -28,11 +34,13 @@ public class OrganizersRecyclerAdapter extends RecyclerView.Adapter<OrganizersRe
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this class
+        public CardView mOrganizerCardView;
         public ImageView mLogoImageView;
         public TextView mNameTextView;
         public TextView mDescriptionTextView;
         public ViewHolder(View view) {
             super(view);
+            mOrganizerCardView = (CardView) view.findViewById(R.id.organizer_card_view);
             mLogoImageView = (ImageView) view.findViewById(R.id.logo_image_view);
             mNameTextView = (TextView) view.findViewById(R.id.name_text_view);
             mDescriptionTextView = (TextView) view.findViewById(R.id.description_text_view);
@@ -62,7 +70,7 @@ public class OrganizersRecyclerAdapter extends RecyclerView.Adapter<OrganizersRe
     public void onBindViewHolder(ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        Organizer organizer = mOrganizersList.get(position);
+        final Organizer organizer = mOrganizersList.get(position);
 
         // Logo
         if (organizer.hasLogoUrl()) {
@@ -78,6 +86,14 @@ public class OrganizersRecyclerAdapter extends RecyclerView.Adapter<OrganizersRe
         if (organizer.hasDescription()) {
             holder.mDescriptionTextView.setText(organizer.getDescription());
         }
+
+        // Set the onClick listener
+        holder.mOrganizerCardView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                launchOrganizerDetailsActivity(organizer.getObjectId());
+            }
+        });
     }
 
     // Return the size of your dataset (invoked by the layout manager)
@@ -86,4 +102,20 @@ public class OrganizersRecyclerAdapter extends RecyclerView.Adapter<OrganizersRe
         return mOrganizersList.size();
     }
 
+    /**
+     * Launch the activity which shows the organizer details
+     * @param organizerObjectId
+     *      The object id of the organizer to be shown
+     */
+    private void launchOrganizerDetailsActivity(String organizerObjectId) {
+        // Precondition: The organizer object id shouldn't be null
+        if (TextUtils.isEmpty(organizerObjectId)) {
+            Log.e(TAG, "Trying to launch the organizer details activity when the object id of the organizer is null");
+            return;
+        }
+
+        Intent startOrganizerDetailsActivityIntent = new Intent(mContext, OrganizerDetailsActivity.class);
+        startOrganizerDetailsActivityIntent.putExtra(Organizer.INTENT_KEY_OBJECT_ID, organizerObjectId);
+        mContext.startActivity(startOrganizerDetailsActivityIntent);
+    }
 }
